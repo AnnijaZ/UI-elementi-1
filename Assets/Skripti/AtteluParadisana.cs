47598b680fad49e84b7ac9383572ab3c3850dcf9
Annija <annija.zorgenberga@gmail.com>
2022-02-22 10:43:52 +0200

    Pievienoti vairaki elementi

--- Assets/Skripti/AtteluParadisana.cs
+++ Assets/Skripti/AtteluParadisana.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AtteluParadisana : MonoBehaviour {
 	public GameObject bins;
 	public GameObject lacitis;
 	public GameObject tante;
 	public GameObject masina;
+	public GameObject attelaTuretajs;
+	public Sprite[] attelumasivs;
+	public GameObject slaideris;
 
 	public void binaAttelosana(bool vertiba){
 		bins.SetActive (vertiba);
@@ -30,5 +34,19 @@ public class AtteluParadisana : MonoBehaviour {
 
 	public void paLabiBins(){
 		bins.transform.localScale = new Vector2 (-1, 1);
+	}
+
+	public void izkritosais(int skaitlis){
+		if (skaitlis == 0)
+			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [0];
+		else if (skaitlis==1)
+			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [1];
+		else if (skaitlis==2)
+			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [2];
+	}
+
+	public void mainitLielumu(){
+		float pasreizejaVertiba = slaideris.GetComponent<Slider> ().value;
+		attelaTuretajs.transform.localScale = new Vector2 (1F * pasreizejaVertiba, 1F * pasreizejaVertiba);
 	}
 }
